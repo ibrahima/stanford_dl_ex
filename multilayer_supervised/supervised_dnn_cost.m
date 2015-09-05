6fbcd4daddeb52be45e43c999296d62703734623
Ibrahim Awwal <ibrahim.awwal@gmail.com>
2015-09-04 18:46:43 -0700

    Fixed a bunch of bugs in supervised DNN gradient/cost code.

--- multilayer_supervised/supervised_dnn_cost.m
+++ multilayer_supervised/supervised_dnn_cost.m
@@ -23,8 +23,11 @@ cost = 0;
 m = size(data,2); % Number of samples
 K = size(stack{numHidden+1}.W, 1); % Number of classes
 D = size(data, 1); % Number of dimensions (length of input vector)
+
 labels_nice = zeros(m, K);
-labels_nice(:, labels) = 1;
+labels_ind = sub2ind(size(labels_nice), 1:m, labels');
+labels_nice(labels_ind) = 1;
+
 nl = numHidden+2;
 pred_prob = zeros(m, K);
 Delta_W = cell(nl, 1);
@@ -97,17 +100,17 @@ delta_l = cell(nl,1);
 % From http://ufldl.stanford.edu/tutorial/supervised/ExerciseSupervisedNeuralNetwork/
 delta_l{nl} = -(labels_nice - pred_prob);
 
-for l = nl-1:-1:2
+for l = nl-1:-1:1
     W_l = stack{l}.W;
     % b_l = stack{l}.b;
     % delta_l{l} = W_l'*(delta_l{l+1}).*(hAct{l+1}.*(1-hAct{l+1}));
     for i = 1:m
-        a_l = hAct{l+1}(i, :);
+        a_l = hAct{l}(i, :)';
         % This line had issues
-        delta_l{l}(i,:) = W_l'*(delta_l{l+1}(i,:).*a_l.*(1-a_l))';
+        delta_l{l}(i,:) = (W_l'*(delta_l{l+1}(i,:)')).*a_l.*(1-a_l);
 
         % Compute gradients and stuff
-        del = delta_l{l+1}(i,:)'*hAct{l}(i,:);
+        del = delta_l{l+1}(i,:)'*a_l';
         Delta_W{l} = Delta_W{l} + del;
         Delta_b{l} = Delta_b{l} + delta_l{l+1}(i,:)';
     end
