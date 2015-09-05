bc990f1a3c9805919e83188032085dc41a5871f7
Ibrahim Awwal <ibrahim.awwal@gmail.com>
2015-09-04 18:14:08 -0700

    Tried to restructure gradient code and fix bugs.
    Alas, bugs are not fixed.

--- multilayer_supervised/supervised_dnn_cost.m
+++ multilayer_supervised/supervised_dnn_cost.m
@@ -20,24 +20,26 @@ gradStack = cell(numHidden+1, 1);
 
 %%% YOUR CODE HERE %%%
 cost = 0;
-m = size(data,2);
-K = size(stack{numHidden+1}.W,1);
+m = size(data,2); % Number of samples
+K = size(stack{numHidden+1}.W, 1); % Number of classes
+D = size(data, 1); % Number of dimensions (length of input vector)
 labels_nice = zeros(m, K);
 labels_nice(:, labels) = 1;
 nl = numHidden+2;
 pred_prob = zeros(m, K);
 Delta_W = cell(nl, 1);
-
 Delta_b = cell(nl, 1);
 
+hAct{1} = zeros(m, D);
 for l=1:numHidden+1
     Delta_W{l} = zeros(size(stack{l}.W));
     Delta_b{l} = zeros(size(stack{l}.b));
+    hAct{l+1} = zeros(m, size(stack{l}.W, 1)); % Space for the thing
 end
 
 for i = 1:m % Loop over all data items
     x = data(:,i);
-    hAct{1} = x;
+    hAct{1}(i,:) = x;
     % Forward pass
     for l = 1:numHidden
         % Compute the activations based on inputs
@@ -47,52 +49,21 @@ for i = 1:m % Loop over all data items
 
         W_l = stack{l}.W;
         b_l = stack{l}.b;
-        a_l = hAct{l};
+        a_l = hAct{l}(i,:)';
 
         z = W_l*a_l + b_l;
-        hAct{l+1} = sigmoid(z);
+        hAct{l+1}(i,:) = sigmoid(z);
     end
 
     % Compute the output layer
     W = stack{nl-1}.W;
     b = stack{nl-1}.b;
 
-    numerator = exp(W*hAct{nl-1}+b);
+    numerator = exp(W*hAct{nl-1}(i,:)'+b);
     denominator = sum(numerator);
     probs = numerator./denominator;
     pred_prob(i,:) = probs;
-    hAct{nl} = probs;
-
-    if ~po
-        cost = cost - log(probs(labels(i)));
-
-        % At the end of forwardprop, hAct{numHidden+2} = the final
-        % activations (non-hidden).
-        % Backprop
-        % δ(nl)=−∑i=1m[(1{y(i)=k}−P(y(i)=k|x(i);θ))]
-        % δ(l)=((W(l))Tδ(l+1))∙f′(z(l))
-        % Compute the desired partial derivatives:
-        % ∇W(l)J(W,b;x,y) = δ(l+1)(a(l))T
-        % ∇b(l)J(W,b;x,y) = δ(l+1)
-
-        delta_l = cell(nl,1);
-        % From http://ufldl.stanford.edu/tutorial/supervised/ExerciseSupervisedNeuralNetwork/
-        delta_l{nl} = -(labels_nice(i,:)' - probs);
-        for l = nl-1:-1:1
-            W_l = stack{l}.W;
-            b_l = stack{l}.b;
-            a_l = hAct{l+1};
-
-            % This line had issues
-            delta_l{l} = W_l'*(delta_l{l+1}.*a_l.*(1-a_l));
-
-            % Compute gradients and stuff
-            Delta_W{l} = Delta_W{l} + delta_l{l+1}*hAct{l}';
-            Delta_b{l} = Delta_b{l} + delta_l{l+1};
-        end
-
-    end
-
+    hAct{nl}(i,:) = probs;
 
 end %% End loop over m data points
 %% return here if only predictions desired.
@@ -104,10 +75,48 @@ end;
 
 %% compute cost
 %%% YOUR CODE HERE %%%
+cost = 0;
+for i=1:m
+    cost = cost - log(pred_prob(i, labels(i)));
+end
+% cost = cost - log(pred_prob(i, labels(i)));
+
 
 %% compute gradients using backpropagation
 %%% YOUR CODE HERE %%%
 
+% At the end of forwardprop, hAct{numHidden+2} = the final
+% activations (non-hidden).
+% Backprop
+% δ(nl)=−∑i=1m[(1{y(i)=k}−P(y(i)=k|x(i);θ))]
+% δ(l)=((W(l))Tδ(l+1))∙f′(z(l))
+% Compute the desired partial derivatives:
+% ∇W(l)J(W,b;x,y) = δ(l+1)(a(l))T
+% ∇b(l)J(W,b;x,y) = δ(l+1)
+delta_l = cell(nl,1);
+% From http://ufldl.stanford.edu/tutorial/supervised/ExerciseSupervisedNeuralNetwork/
+delta_l{nl} = -(labels_nice - pred_prob);
+
+for l = nl-1:-1:2
+    W_l = stack{l}.W;
+    % b_l = stack{l}.b;
+    % delta_l{l} = W_l'*(delta_l{l+1}).*(hAct{l+1}.*(1-hAct{l+1}));
+    for i = 1:m
+        a_l = hAct{l+1}(i, :);
+        % This line had issues
+        delta_l{l}(i,:) = W_l'*(delta_l{l+1}(i,:).*a_l.*(1-a_l))';
+
+        % Compute gradients and stuff
+        del = delta_l{l+1}(i,:)'*hAct{l}(i,:);
+        Delta_W{l} = Delta_W{l} + del;
+        Delta_b{l} = Delta_b{l} + delta_l{l+1}(i,:)';
+    end
+end
+
+% for l = nl-1:-1:1
+%     Delta_W{l} = delta_l{l+1}*hAct{l}';
+%     Delta_b{l} = delta_l{l+1};
+% end
 % Use Delta_W and Delta_b to compute gradients
 for l = 1:numHidden+1
     gradStack{l}.W = Delta_W{l};
